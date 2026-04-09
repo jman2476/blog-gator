f1177299eb20cb5596e3c92eb1a50f6719a9a4a1
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-04-09 12:47:38 -0400

    add handlerFollow cmd

--- follow.go
+++ follow.go
@@ -1,10 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jman2476/blog-gator/internal/database"
+)
 
 func handlerFollow(s *state, cmd command) error {
 	if len(cmd.arguments) < 1 {
 		return fmt.Errorf("Need a url for the feed")
 	}
 
+	user, err := s.db.GetUser(
+		context.Background(),
+		sql.NullString{
+			String: s.config.Username,
+			Valid:  true,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	feed, err := s.db.GetFeedByURL(
+		context.Background(),
+		sql.NullString{
+			String: cmd.arguments[0],
+			Valid:  true,
+		},
+	)
+	if err != nil {
+		return nil
+	}
+
+	feedfollowArgs := database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	}
+
+	ff, err := s.db.CreateFeedFollow(
+		context.Background(),
+		feedfollowArgs,
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s :: %s", ff.UserName.String, ff.FeedName.String)
+	return nil
 }
