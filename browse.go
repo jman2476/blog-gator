589d4e1951c9e7c7eeabcdc92b20186340d7e6d0
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-04-10 18:00:33 -0400

    add browse command

---
+++ browse.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/jman2476/blog-gator/internal/database"
+)
+
+func handlerBrowse(s *state, cmd command, user database.User) error {
+	var limit int32 = 2
+	if len(cmd.arguments) > 0 {
+		userLimit, err := strconv.Atoi(cmd.arguments[0])
+		if err != nil {
+			return fmt.Errorf("Post limit must be an integer: %w", err)
+		}
+		limit = int32(userLimit)
+	}
+
+	browseArgs := database.GetPostsByUserParams{
+		ID:    user.ID,
+		Limit: limit,
+	}
+
+	posts, err := s.db.GetPostsByUser(context.Background(), browseArgs)
+	if err != nil {
+		return fmt.Errorf("Error browsing, getting posts: %w", err)
+	}
+
+	for _, post := range posts {
+		printPost(post)
+	}
+
+	return nil
+}
+
+func printPost(post database.Post) error {
+	fmt.Printf("<<%s>>\n", post.Title)
+	fmt.Printf("Pub date: %s\n", post.PublishedAt.Time)
+	fmt.Printf("link: %s\n", post.Url)
+	fmt.Printf("Text: %s\n", post.Description.String)
+	fmt.Println("=====================================")
+	return nil
+}
